487e1e033fc2d124b5319516c22e151fd2d9735c
kldhs <listom@sina.com>
2023-02-16 17:41:44 +0800

    refactor: 修改Date工具类

--- iot-common/util/src/main/java/com/iot/common/util/DateUtil.java
+++ iot-common/util/src/main/java/com/iot/common/util/DateUtil.java
@@ -11,26 +11,35 @@ import java.util.Date;
 import java.util.Objects;
 
 /**
- *     LocalDateTime：本地日期时间
- *     OffsetDateTime：带偏移量的日期时间
- *     ZonedDateTime：带时区的日期时间
+ * LocalDateTime：本地日期时间
+ * OffsetDateTime：带偏移量的日期时间
+ * ZonedDateTime：带时区的日期时间
  */
 public class DateUtil {
     private DateUtil() {
     }
 
     public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-    private static final DateTimeFormatter FORMATTER2 = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    public static final DateTimeFormatter FORMATTER2 = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
-    private static final DateTimeFormatter OFFSET_DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssZZZZZ");
-    private static final DateTimeFormatter OFFSET_DATE_TIME_FORMATTER2 = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mmZZZZZ");
-    private static final DateTimeFormatter OFFSET_DATE_TIME_FORMATTER3 = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ssZZZZZ");
+    public static final DateTimeFormatter OFFSET_DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssZZZZZ");
+    public static final DateTimeFormatter OFFSET_DATE_TIME_FORMATTER2 = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mmZZZZZ");
+    public static final DateTimeFormatter OFFSET_DATE_TIME_FORMATTER3 = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ssZZZZZ");
 
-    public static String getNowStr() {
+    /**
+     * 获取当前LocalDateTime字符串
+     * @return
+     */
+    public static String getNowLocalDateTimeString() {
         return LocalDateTime.now().format(DATE_TIME_FORMATTER);
     }
 
-    public static LocalDateTime dateToJava8Type(Date date) {
+    /**
+     * Date转LocalDateTime
+     * @param date
+     * @return
+     */
+    public static LocalDateTime dateToLocalDateTime(Date date) {
         if (date == null) {
             return null;
         }
@@ -38,68 +47,111 @@ public class DateUtil {
         return instant.atZone(ZoneId.systemDefault()).toLocalDateTime();
     }
 
-    public static Date java8TypeToDate(LocalDateTime time) {
+    /**
+     * LocalDateTime转Date
+     * @param time
+     * @return
+     */
+    public static Date localDateTimeToDate(LocalDateTime time) {
         if (time == null) return null;
         var instant = time.atZone(ZoneId.systemDefault()).toInstant();
         return Date.from(instant);
     }
 
-    public static LocalDateTime tsMilliToLocalDateTime(Long ts) {
+    /**
+     * 毫秒转LocalDateTime
+     * @param ts
+     * @return
+     */
+    public static LocalDateTime milliToLocalDateTime(Long ts) {
         return LocalDateTime.ofInstant(Instant.ofEpochMilli(ts), ZoneId.systemDefault());
     }
 
-    public static OffsetDateTime tsMilliToOffsetDateTime(Long ts) {
+    /**
+     * 毫秒转OffsetDateTime
+     * @param ts
+     * @return
+     */
+    public static OffsetDateTime milliToOffsetDateTime(Long ts) {
         return OffsetDateTime.ofInstant(Instant.ofEpochMilli(ts), ZoneId.systemDefault());
     }
 
+    /**
+     * LocalDateTime转毫秒
+     * @param localDateTime
+     * @return
+     */
+    public static Long localDateTimeToMilli(LocalDateTime localDateTime) {
+        return localDateTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
+    }
+
+    /**
+     * LocalDateTime转OffsetDateTime
+     * @param localDateTime
+     * @return
+     */
     public static OffsetDateTime localToOffsetDateTime(LocalDateTime localDateTime) {
         if (Objects.isNull(localDateTime)) return null;
         return OffsetDateTime.ofInstant(localDateTime.atZone(ZoneId.systemDefault()).toInstant(), ZoneId.systemDefault());
     }
 
+    /**
+     * OffsetDateTime转
+     * @param offsetDateTime
+     * @return
+     */
     public static LocalDateTime offsetToLocalDateTime(OffsetDateTime offsetDateTime) {
         if (Objects.isNull(offsetDateTime)) return null;
         return offsetDateTime.atZoneSameInstant(ZoneId.systemDefault()).toLocalDateTime();
     }
 
-    public static OffsetDateTime parseOffsetTime(String time) {
+    /**
+     * OffsetDateTime格式的字符串转OffsetDateTime
+     * @param time
+     * @return
+     */
+    public static OffsetDateTime stringToOffsetDateTime(String time) {
         try {
             return OffsetDateTime.parse(time, OFFSET_DATE_TIME_FORMATTER);
         } catch (DateTimeParseException ignored) {
             //ignore
         }
-
         try {
             return OffsetDateTime.parse(time, OFFSET_DATE_TIME_FORMATTER2);
         } catch (DateTimeParseException ignored) {
             //ignore
         }
-
         throw new IllegalArgumentException("time format parse error");
     }
 
-    public static Long stringToTimestamp(String timeString) {
+    /**
+     * LocalDateTime格式的字符串转LocalDateTime
+     * @param timeString
+     * @return
+     */
+    public static LocalDateTime stringToLocalDateTime(String timeString) {
         try {
-            return LocalDateTime.parse(timeString, DATE_TIME_FORMATTER).toInstant(OffsetDateTime.now().getOffset()).toEpochMilli();
+            return LocalDateTime.parse(timeString, DATE_TIME_FORMATTER);
         } catch (DateTimeParseException ignored) {
             //ignore
         }
         try {
-            return LocalDateTime.parse(timeString, FORMATTER2).toInstant(OffsetDateTime.now().getOffset()).toEpochMilli();
+            return LocalDateTime.parse(timeString, FORMATTER2);
         } catch (DateTimeParseException ignored) {
             //ignore
         }
         throw new IllegalArgumentException("time format error");
     }
 
-    public static int calculateMonthsBetweenTwoStr(String fromDate, String toDate) {
-        var fromLocalDateTime = LocalDateTime.parse(fromDate, DATE_TIME_FORMATTER);
-        var toLocalDateTime = LocalDateTime.parse(toDate, DATE_TIME_FORMATTER);
-        return (int) ChronoUnit.MONTHS.between(fromLocalDateTime, toLocalDateTime);
+    /**
+     * 计算两个LocalDateTime之间相差的月份
+     * @param fromDate
+     * @param toDate
+     * @return
+     */
+    public static int calculateMonthsBetweenLocalDateTime(LocalDateTime fromDate, LocalDateTime toDate) {
+        return (int) ChronoUnit.MONTHS.between(fromDate, toDate);
     }
 
-    public static Long localToMillisecond(LocalDateTime localDateTime) {
-        return localDateTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
-    }
 
 }
